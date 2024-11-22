b24657732503900137a6f7875fab87e44e2063b9
cyber-Ager <ageuriazp@gmail.com>
2024-11-22 12:54:06 +0100

    Gehitu dot "dplyr" kargatzea lehenengo %>%- ulertzeko.

--- R/precission_error_calculation.R
+++ R/precission_error_calculation.R
@@ -38,6 +38,7 @@ measure_precision <- function(tb, img = TRUE){
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) install.packages(new.packages)
 
+  library(dplyr)
 
   # FUNCTION STARTS HERE-------------------------------------------------------
 
